2cd5d14ddfa782739846123d01e98d542581d66d
Bob Ying <124599792@qq.com>
2019-01-23 13:14:29 +0800

    missing config files

---
+++ XT_CETC23/Config/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XT_CETC23.Config
+{
+    class Config
+    {
+        static public bool ENABLED_VISIONPRO = false;
+    }
+}
