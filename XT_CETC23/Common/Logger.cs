a676723e6f43e8306413be1af52f0e4c8fc8d8aa
yingmingbo <124599792@qq.com>
2019-02-12 15:36:39 +0800

    增加日志工具

---
+++ XT_CETC23/Common/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XT_CETC23
+{
+    class Logger
+    {
+        static public void printException(Exception e)
+        {
+            Console.WriteLine(" ***** " + e.Message);
+            Console.WriteLine(" ***** " + e.StackTrace);
+        }
+    }
+}
