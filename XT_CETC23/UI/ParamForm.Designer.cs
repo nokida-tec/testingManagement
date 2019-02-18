ffeda16f1ea040e6c22334f057d639426121e6c8
yingmingbo <124599792@qq.com>
2019-02-18 21:20:15 +0800

    调整参数配置界面布局

--- XT_CETC23/UI/ParamForm.Designer.cs
+++ XT_CETC23/UI/ParamForm.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGV_Product = new System.Windows.Forms.DataGridView();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serialNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.productDefBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dB23DataSet = new XT_CETC23.DB23DataSet();
             this.btnSaveProdCode = new System.Windows.Forms.Button();
@@ -43,9 +46,6 @@
             this.btnSaveProdStatus = new System.Windows.Forms.Button();
             this.operateDefTableAdapter = new XT_CETC23.DB23DataSetTableAdapters.OperateDefTableAdapter();
             this.productDefTableAdapter = new XT_CETC23.DB23DataSetTableAdapters.ProductDefTableAdapter();
-            this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.serialNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGV_Product)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productDefBindingSource)).BeginInit();
@@ -61,11 +61,11 @@
             this.groupBox1.Controls.Add(this.btnSaveProdCode);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.groupBox1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(15);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(15);
-            this.groupBox1.Size = new System.Drawing.Size(506, 423);
+            this.groupBox1.Size = new System.Drawing.Size(481, 443);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "产品编码配置";
@@ -85,8 +85,26 @@
             this.dataGV_Product.Location = new System.Drawing.Point(15, 37);
             this.dataGV_Product.Name = "dataGV_Product";
             this.dataGV_Product.RowTemplate.Height = 23;
-            this.dataGV_Product.Size = new System.Drawing.Size(476, 336);
+            this.dataGV_Product.Size = new System.Drawing.Size(451, 356);
             this.dataGV_Product.TabIndex = 3;
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "产品类型";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // serialNoDataGridViewTextBoxColumn
+            // 
+            this.serialNoDataGridViewTextBoxColumn.DataPropertyName = "SerialNo";
+            this.serialNoDataGridViewTextBoxColumn.HeaderText = "产品编号";
+            this.serialNoDataGridViewTextBoxColumn.Name = "serialNoDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn1
+            // 
+            this.nameDataGridViewTextBoxColumn1.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn1.HeaderText = "产品名称";
+            this.nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
             // 
             // productDefBindingSource
             // 
@@ -102,10 +120,10 @@
             // 
             this.btnSaveProdCode.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnSaveProdCode.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnSaveProdCode.Location = new System.Drawing.Point(15, 373);
+            this.btnSaveProdCode.Location = new System.Drawing.Point(15, 393);
             this.btnSaveProdCode.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
             this.btnSaveProdCode.Name = "btnSaveProdCode";
-            this.btnSaveProdCode.Size = new System.Drawing.Size(476, 35);
+            this.btnSaveProdCode.Size = new System.Drawing.Size(451, 35);
             this.btnSaveProdCode.TabIndex = 2;
             this.btnSaveProdCode.Text = "保  存";
             this.btnSaveProdCode.UseVisualStyleBackColor = true;
@@ -117,11 +135,11 @@
             this.groupBox2.Controls.Add(this.btnSaveProdStatus);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
             this.groupBox2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.groupBox2.Location = new System.Drawing.Point(529, 20);
+            this.groupBox2.Location = new System.Drawing.Point(507, 10);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(15);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(15);
-            this.groupBox2.Size = new System.Drawing.Size(523, 423);
+            this.groupBox2.Size = new System.Drawing.Size(471, 443);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "产品状态配置";
@@ -141,7 +159,7 @@
             this.dataGV_Operate.Location = new System.Drawing.Point(15, 38);
             this.dataGV_Operate.Name = "dataGV_Operate";
             this.dataGV_Operate.RowTemplate.Height = 23;
-            this.dataGV_Operate.Size = new System.Drawing.Size(493, 335);
+            this.dataGV_Operate.Size = new System.Drawing.Size(441, 355);
             this.dataGV_Operate.TabIndex = 2;
             // 
             // productTypeDataGridViewTextBoxColumn
@@ -171,9 +189,9 @@
             // 
             this.btnSaveProdStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnSaveProdStatus.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnSaveProdStatus.Location = new System.Drawing.Point(15, 373);
+            this.btnSaveProdStatus.Location = new System.Drawing.Point(15, 393);
             this.btnSaveProdStatus.Name = "btnSaveProdStatus";
-            this.btnSaveProdStatus.Size = new System.Drawing.Size(493, 35);
+            this.btnSaveProdStatus.Size = new System.Drawing.Size(441, 35);
             this.btnSaveProdStatus.TabIndex = 1;
             this.btnSaveProdStatus.Text = "保  存";
             this.btnSaveProdStatus.UseVisualStyleBackColor = true;
@@ -187,36 +205,18 @@
             // 
             this.productDefTableAdapter.ClearBeforeFill = true;
             // 
-            // nameDataGridViewTextBoxColumn1
-            // 
-            this.nameDataGridViewTextBoxColumn1.DataPropertyName = "Name";
-            this.nameDataGridViewTextBoxColumn1.HeaderText = "产品名称";
-            this.nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
-            // 
-            // serialNoDataGridViewTextBoxColumn
-            // 
-            this.serialNoDataGridViewTextBoxColumn.DataPropertyName = "SerialNo";
-            this.serialNoDataGridViewTextBoxColumn.HeaderText = "产品编号";
-            this.serialNoDataGridViewTextBoxColumn.Name = "serialNoDataGridViewTextBoxColumn";
-            // 
-            // typeDataGridViewTextBoxColumn
-            // 
-            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
-            this.typeDataGridViewTextBoxColumn.HeaderText = "产品类型";
-            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
-            // 
             // ParamForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1072, 463);
+            this.ClientSize = new System.Drawing.Size(988, 463);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ParamForm";
-            this.Padding = new System.Windows.Forms.Padding(20);
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "ParamForm";
