a84853d3699d5f63f6f016ba8130909c2cfcfb0a
yingmingbo <124599792@qq.com>
2019-02-23 20:09:28 +0800

    通用等待机制

---
+++ XT_CETC23/Common/WaitCondition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XT_CETC23
+{
+    class WaitCondition
+    {
+        public delegate bool Condition();
+
+        public static ReturnCode waitCondition(Condition condition)
+        {
+            while (!condition())
+            {
+                if (TestingSystem.GetInstanse().isSystemExisting() == true)
+                {
+                    return ReturnCode.SystemExiting;
+                }
+                Thread.Sleep(100);
+            }
+            return ReturnCode.OK;
+        }
+    }
+}
