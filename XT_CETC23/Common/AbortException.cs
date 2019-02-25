6eb2ff195fb280ce289b64dbfdb12662b300018d
yingmingbo <124599792@qq.com>
2019-02-25 21:28:35 +0800

    添加自定义退出异常

---
+++ XT_CETC23/Common/AbortException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XT_CETC23
+{
+    class AbortException : Exception
+    {
+        public AbortException (String message)
+            : base(message)
+        {
+        }
+    }
+}
