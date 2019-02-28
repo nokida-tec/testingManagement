fccc0e29098afb8aacb05e6a9a3dbc7e03b06e29
yingmingbo <124599792@qq.com>
2019-02-28 17:08:10 +0800

    需要报警的异常

---
+++ XT_CETC23/Common/AlarmException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XT_CETC23
+{
+    class AlarmException : Exception
+    {
+        public AlarmException(String message)
+            : base(message)
+        {
+        }
+    }
+}
