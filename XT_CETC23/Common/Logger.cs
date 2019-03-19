87701af8e388abb455a72cdafcb4737ec30a2fb3
yingmingbo <124599792@qq.com>
2019-03-19 09:00:43 +0800

    日志输出到文件及屏幕

--- XT_CETC23/Common/Logger.cs
+++ XT_CETC23/Common/Logger.cs
@@ -3,29 +3,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using XT_CETC23.DataManager;
+using XT_CETC23.Common;
+using XT_CETC23.Model;
+using XT_CETC23.DataCom;
+using XT_CETC23.Instances;
 
 namespace XT_CETC23
 {
     class Logger
     {
-        static public void printException(Exception e)
+        static Logger sInstance = null;
+        static Object sLockInstance = new Object();
+
+        static public Logger getInstance()
         {
-            Console.WriteLine(" ***** " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ") + e.Message);
-            Console.WriteLine(" ***** " + e.StackTrace);
+            if (sInstance == null)
+            {
+                lock (sLockInstance)
+                {
+                    if (sInstance == null)
+                    {
+                        sInstance = new Logger();
+                    }
+                }
+            }
+            return sInstance;
         }
 
-        static public void WriteLine(Exception e)
+        private StreamWriter mStreamWriter = null;
+        public delegate void delegateShowMessage(string message);
+        private delegateShowMessage mDelegateOfShow = null;
+
+        private Logger ()
         {
-            Console.WriteLine(" ***** " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ") + e.Message);
-            Console.WriteLine(" ***** " + e.StackTrace);
+            try
+            {
+                mStreamWriter = File.AppendText(DataBase.logPath + @"\\syslog-" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".txt");
+            }
+            catch (Exception e)
+            {
+                writeLine(e);
+                
+                for (Char disk = 'f'; disk >= 'c'; disk --)
+                {
+                    try
+                    {
+                        mStreamWriter = File.AppendText(disk + @":\syslog-" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".txt");
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+            finally
+            {
+                Console.SetOut(mStreamWriter);
+            }
         }
 
-        static public void WriteLine(String message)
+        public void RegistryDelegate(delegateShowMessage delegateOfShow)
         {
-            Console.WriteLine(" ***** " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ")  + message);
+            mDelegateOfShow = delegateOfShow;
         }
 
-        static public void WriteLineWithStack(String message)
+        public void UnregistryDelegate(delegateShowMessage delegateOfShow)
+        {
+            mDelegateOfShow = null;
+        }
+
+        public void writeLine(Exception e)
+        {
+            writeLine(true, e);
+        }
+
+        public void writeLine(bool showInScreen, Exception e)
+        {
+            try
+            {
+                String line = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ") + e.Message;
+                Console.WriteLine(" ***** " + line);
+                Console.WriteLine(" ***** " + e.StackTrace);
+                mStreamWriter.Flush();
+                if (showInScreen && mDelegateOfShow != null)
+                {
+                    mDelegateOfShow(line);
+                    mDelegateOfShow(e.StackTrace);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void writeLine(String message)
+        {
+            writeLine(true, message);
+        }
+
+        public void writeLine(bool showInScreen, String message)
+        {
+            try
+            {
+                String line = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ") + message;
+                Console.WriteLine(" ***** " + line);
+                mStreamWriter.Flush();
+                if (showInScreen && mDelegateOfShow != null)
+                {
+                    mDelegateOfShow(line);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void writeLineWithStack(String message)
         {
             try
             {
@@ -33,8 +130,24 @@ namespace XT_CETC23
             }
             catch (Exception e)
             {
-                WriteLine(e);
+                writeLine(e);
             }
         }
+
+        static public void WriteLine(Exception e)
+        {
+            Logger.getInstance().writeLine(e);
+        }
+
+        static public void WriteLine(String message)
+        {
+            Logger.getInstance().writeLine(message);
+        }
+
+        static public void WriteLineWithStack(String message)
+        {
+            Logger.getInstance().writeLineWithStack(message);
+        }
+
     }
 }
