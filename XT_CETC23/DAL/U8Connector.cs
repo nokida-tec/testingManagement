dfaa0729a2d378df911d9bebe44af659f25e72dc
yingmingbo <124599792@qq.com>
2019-03-19 08:50:51 +0800

    独立U8对接

---
+++ XT_CETC23/DAL/U8Connector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
+using System.Data;
+using System.IO;
+using XT_CETC23.DataManager;
+using XT_CETC23.Common;
+using XT_CETC23.Model;
+using XT_CETC23.Instances;
+using Excel;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace XT_CETC23
+{
+    class U8Connector
+    {
+        // 
+        [DllImport("user32.dll", EntryPoint = "FindWindowA", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowExA", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, uint hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageA", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", EntryPoint = "PostMessageA", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int PostMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        private const int WM_SETTEXT = 0x000C;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int vbKeyReturn = 0x0D;
+
+        public static int sendToU8(string content)
+        {
+            IntPtr hwndWindow = FindWindow(null, "序列号专用解析方案"); // find u8 dialog
+
+            if (hwndWindow != IntPtr.Zero)
+            {
+                IntPtr hwndInput = FindWindowEx(hwndWindow, 0, "ThunderRT6TextBox", null);
+                if (hwndInput != IntPtr.Zero)
+                {
+                    IntPtr text = Marshal.StringToHGlobalAnsi(content);
+                    int ret = SendMessage(hwndInput, WM_SETTEXT, IntPtr.Zero, text);
+                    int errCode = Marshal.GetLastWin32Error();
+                    Logger.WriteLine(new System.ComponentModel.Win32Exception(errCode).Message);
+                    Marshal.FreeCoTaskMem(text);
+                    ret = PostMessage(hwndInput, WM_KEYDOWN, (IntPtr)vbKeyReturn, IntPtr.Zero);
+                    ret = PostMessage(hwndInput, WM_KEYUP, (IntPtr)vbKeyReturn, IntPtr.Zero);
+                    return 1;
+                }
+                else
+                {
+                    Logger.WriteLine("请确认U8转移报工窗口打开");
+                    return -2;
+                }
+            }
+            else
+            {
+                Logger.WriteLine("请确认U8转移报工窗口打开");
+                return -1;
+            }
+            return 0;
+        }
+
+    }
+}
